ffbe80e0480a8a5c26ec8bf8700d4d1bcb3919f6
Ravis-111 <ravindras111@gmail.com>
2026-05-24 15:52:20 +0530

    Add files

--- workflows/pyspark_dag.py
+++ workflows/pyspark_dag.py
@@ -13,7 +13,7 @@ from airflow.providers.google.cloud.operators.dataproc import (
 PROJECT_ID = "top-gantry-489113-j4"
 REGION = "us-east1"
 CLUSTER_NAME = "my-demo-cluster"
-COMPOSER_BUCKET = "us-central1-demo-instance-3eff17e9-bucket" ## change this 
+COMPOSER_BUCKET = "us-central1-demo-instance-98e37539-bucket" ## change this 
 
 GCS_JOB_FILE_1 = f"gs://{COMPOSER_BUCKET}/data/INGESTION/hospitalA_mysqlToLanding.py"
 PYSPARK_JOB_1 = {
